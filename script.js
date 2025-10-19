180b0cfac4a7eaed440f5a436ec9c030fd8542e4
Shreya Singh <143871325+shreyaa-offgrid@users.noreply.github.com>
2025-10-19 12:31:44 +0000

    Write createCard function

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-const myLibrary = [];
-
 function Book(title,author,pages,read,id) {
-  // the constructor...
   this.title = title;
   this.author = author;
   this.pages = pages;
@@ -9,16 +6,36 @@ function Book(title,author,pages,read,id) {
   this.id = id;
 }
 
+const myLibrary = [];
+
 function addBookToLibrary(title,author,pages,read) {
   // take params, create a book then store it in the array
   let id =  crypto.randomUUID();
   let newBook = new Book(title,author,pages,read,id);
   myLibrary.push(newBook); 
+  createCard(newBook);
 }
 
-function displayBooks(){
-    myLibrary.forEach(function(book){
-        let book = document.createElement("p");
-        book.textContent = book.title;
-    })
+function createCard(book){
+  let card = document.createElement("div");
+  let bookname = document.createElement("h3");
+  let authorname = document.createElement("p");
+  let pagesinbook = document.createElement("p");
+  bookname.textContent = book.title;
+  authorname.textContent = "By "+book.author;
+  pagesinbook.textContent = book.pages;
+  card.classList.add("card");
+  if(book.read){
+    card.classList.add("read");
+  }else{
+    card.classList.add("unread");
+  }
+  card.appendChild(bookname);
+  card.appendChild(authorname);
+  card.appendChild(pagesinbook);
+  const gridcontainer = document.querySelector(".main");
+  gridcontainer.appendChild(card);
 }
+
+
+
