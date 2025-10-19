2970b3526311266d62ead1f6528a9ddd1f469e95
Shreya Singh <143871325+shreyaa-offgrid@users.noreply.github.com>
2025-10-19 10:05:52 +0000

    Create basic layout, choose fonts and colors

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-const heading = document.querySelector("h1");
-heading.addEventListener("click", function(e){
-    heading.style.color = "blue";
-})
+const myLibrary = [];
+
+function Book(title,author,pages,read,id) {
+  // the constructor...
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.id = id;
+}
+
+function addBookToLibrary(title,author,pages,read) {
+  // take params, create a book then store it in the array
+  let id =  crypto.randomUUID();
+  let newBook = new Book(title,author,pages,read,id);
+  myLibrary.push(newBook); 
+}
+
+function displayBooks(){
+    myLibrary.forEach(function(book){
+        let book = document.createElement("p");
+        book.textContent = book.title;
+    })
+}
